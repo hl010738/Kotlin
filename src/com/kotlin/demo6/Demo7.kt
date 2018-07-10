a9b22115dcfad69b8aac9bdf37f00f1978175dc0
hl010738 <hl010738@126.com>
2018-07-10 17:27:29 +0800

    Lamda表达式

---
+++ src/com/kotlin/demo6/Demo7.kt
@@ -0,0 +1,31 @@
+package com.kotlin.demo6
+
+//Lamda表达式列子
+
+fun String.filter(predicate: (Char) -> Boolean): String{
+    var buff = StringBuffer()
+    for (index in 0 until length){
+        var c = get(index)
+        if(predicate(c)){
+            buff.append(c)
+        }
+    }
+    return buff.toString()
+
+}
+
+fun main(args: Array<String>) {
+
+    var str = "abc123"
+
+    println(str.filter { it -> it.isLetter() })
+
+    println("------------------")
+
+    println(str.filter { it ->
+        when(it){
+            '1', '2', '3' -> true
+            else -> false
+        }
+    })
+}
