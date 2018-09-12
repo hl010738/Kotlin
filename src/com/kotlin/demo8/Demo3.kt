3b3a0b817adad0126fcc2b9e0b8925f639041ceb
hl010738 <hl010738@126.com>
2018-09-12 17:35:28 +0800

    数组

---
+++ src/com/kotlin/demo8/Demo3.kt
@@ -0,0 +1,10 @@
+package com.kotlin.demo8
+
+/*
+    数组
+ */
+
+fun main(args: Array<String>) {
+    //val intArray: Array<Int> = intArrayOf(1, 2, 3)
+    val intArray2: Array<Int> = arrayOf(1, 2, 3)
+}
