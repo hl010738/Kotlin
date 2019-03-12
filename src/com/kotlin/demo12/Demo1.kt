a17831f7fee4666845cb70485b06166feb014daa
talent_mbp2018 <hl010738@126.com>
2019-03-12 14:48:58 +0800

    Java 代码 调用Kotlin方法，传入null参数

---
+++ src/com/kotlin/demo12/Demo1.kt
@@ -0,0 +1,8 @@
+package com.kotlin.demo12
+
+class Demo1{
+    fun method(str: String){
+        println("method invoked")
+        println(str)
+    }
+}
---
+++ src/com/kotlin/demo12/Test1.java
@@ -0,0 +1,9 @@
+package com.kotlin.demo12;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        demo1.method("123");
+        demo1.method(null);
+    }
+}
